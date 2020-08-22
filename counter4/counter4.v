164a7edf71d5a55bcab052d3d1b9b80e32442995
jaymzee <jaymzee@users.noreply.github.com>
2020-08-22 07:52:48 -0400

    renamed 4 bit counter to counter4

--- counter4/counter4.v
+++ counter4/counter4.v
@@ -1,8 +1,8 @@
-module counter (
+module counter4 (
     input wire clk,
     input wire rst,
     input wire en,
-    input wire dir,
+    input wire up,
     output reg [3:0] count
 );
 
@@ -11,7 +11,7 @@ always @(posedge clk, posedge rst)
         count = #0.5 0;
     else
         if (en)
-            if (dir)
+            if (up)
                 count = #0.5 count + 1;
             else
                 count = #0.5 count - 1;
--- counter4/counter4_tb.v
+++ counter4/counter4_tb.v
@@ -1,37 +1,38 @@
 `timescale 10ns / 1ns
-module counter_tb();
+module counter4_tb();
 
-reg clk, rst, en, dir;
+reg clk, rst, en, up;
 wire [3:0] count1, count2;
-wire #0.5 dirc = ~dir;
+wire #0.5 up_ = ~up;
 
-counter U1(
+counter4 U1(
     .clk(clk),
     .rst(rst),
     .en(en),
-    .dir(dir),
+    .up(up),
     .count(count1)
 );
 
-counter U2(
+counter4 U2(
     .clk(clk),
     .rst(rst),
     .en(en),
-    .dir(dirc),
+    .up(up_),
     .count(count2)
 );
 
 initial begin
-    $dumpfile("counter.vcd");
+    $dumpfile("counter4.vcd");
     $dumpvars(1, U1);
+    $dumpvars(1, U2);
     $monitor("rst %b en %b dir %b cnt1 %b cnt2 %b",
-              rst, en, dir, count1, count2);
+              rst, en, up, count1, count2);
     clk = 0;
-    dir = 1;
+    up = 1;
     en = 1;
     rst = 1;
 #10 rst = 0;
-#100 dir = 0;
+#100 up = 0;
 #80  rst = 1;
 #10  $finish;
 end
