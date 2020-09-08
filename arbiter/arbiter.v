df77793cc7e21ecd70dce9319700d1156288fb89
jaymzee <jaymzee@users.noreply.github.com>
2020-09-08 10:29:26 -0400

    arbiter

--- arbiter/arbiter.v
+++ arbiter/arbiter.v
@@ -3,72 +3,72 @@
 // orginally coded by WD Peterson in VHDL.
 //----------------------------------------------------
 module arbiter (
-  clk,    
-  rst,    
-  req3,   
-  req2,   
-  req1,   
-  req0,   
-  gnt3,   
-  gnt2,   
-  gnt1,   
-  gnt0   
+    clk,
+    rst,
+    req3,
+    req2,
+    req1,
+    req0,
+    gnt3,
+    gnt2,
+    gnt1,
+    gnt0
 );
 // --------------Port Declaration----------------------- 
-input           clk;    
-input           rst;    
-input           req3;   
-input           req2;   
-input           req1;   
-input           req0;   
-output          gnt3;   
-output          gnt2;   
-output          gnt1;   
-output          gnt0;   
+input           clk;
+input           rst;
+input           req3;
+input           req2;
+input           req1;
+input           req0;
+output          gnt3;
+output          gnt2;
+output          gnt1;
+output          gnt0;
 
 //--------------Internal Registers----------------------
-wire    [1:0]   gnt       ;   
-wire            comreq    ; 
-wire            beg       ;
-wire   [1:0]    lgnt      ;
-wire            lcomreq   ;
-reg             lgnt0     ;
-reg             lgnt1     ;
-reg             lgnt2     ;
-reg             lgnt3     ;
-reg             lasmask   ;
-reg             lmask0    ;
-reg             lmask1    ;
-reg             ledge     ;
+wire    [1:0]   gnt;
+wire            comreq;
+wire            beg;
+wire    [1:0]   lgnt;
+wire            lcomreq;
+reg             lgnt0;
+reg             lgnt1;
+reg             lgnt2;
+reg             lgnt3;
+reg             lasmask;
+reg             lmask0;
+reg             lmask1;
+reg             ledge;
 
 //--------------Code Starts Here----------------------- 
 always @ (posedge clk)
 if (rst) begin
-  lgnt0 <= 0;
-  lgnt1 <= 0;
-  lgnt2 <= 0;
-  lgnt3 <= 0;
-end else begin                                     
-  lgnt0 <=(~lcomreq & ~lmask1 & ~lmask0 & ~req3 & ~req2 & ~req1 & req0)
-        | (~lcomreq & ~lmask1 &  lmask0 & ~req3 & ~req2 &  req0)
-        | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req0)
-        | (~lcomreq &  lmask1 &  lmask0 & req0  )
-        | ( lcomreq & lgnt0 );
-  lgnt1 <=(~lcomreq & ~lmask1 & ~lmask0 &  req1)
-        | (~lcomreq & ~lmask1 &  lmask0 & ~req3 & ~req2 &  req1 & ~req0)
-        | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req1 & ~req0)
-        | (~lcomreq &  lmask1 &  lmask0 &  req1 & ~req0)
-        | ( lcomreq &  lgnt1);
-  lgnt2 <=(~lcomreq & ~lmask1 & ~lmask0 &  req2  & ~req1)
-        | (~lcomreq & ~lmask1 &  lmask0 &  req2)
-        | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req2  & ~req1 & ~req0)
-        | (~lcomreq &  lmask1 &  lmask0 &  req2 & ~req1 & ~req0)
-        | ( lcomreq &  lgnt2);
-  lgnt3 <=(~lcomreq & ~lmask1 & ~lmask0 & req3  & ~req2 & ~req1)
-        | (~lcomreq & ~lmask1 &  lmask0 & req3  & ~req2)
-        | (~lcomreq &  lmask1 & ~lmask0 & req3)
-        | (~lcomreq &  lmask1 &  lmask0 & req3  & ~req2 & ~req1 & ~req0)
-        | ( lcomreq & lgnt3);
+    lgnt0 <= 0;
+    lgnt1 <= 0;
+    lgnt2 <= 0;
+    lgnt3 <= 0;
+end else begin
+    lgnt0 <= (~lcomreq & ~lmask1 & ~lmask0 & ~req3 & ~req2 & ~req1 & req0)
+           | (~lcomreq & ~lmask1 &  lmask0 & ~req3 & ~req2 &  req0)
+           | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req0)
+           | (~lcomreq &  lmask1 &  lmask0 & req0  )
+           | ( lcomreq & lgnt0 );
+    lgnt1 <= (~lcomreq & ~lmask1 & ~lmask0 &  req1)
+           | (~lcomreq & ~lmask1 &  lmask0 & ~req3 & ~req2 &  req1 & ~req0)
+           | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req1 & ~req0)
+           | (~lcomreq &  lmask1 &  lmask0 &  req1 & ~req0)
+           | ( lcomreq &  lgnt1);
+    lgnt2 <= (~lcomreq & ~lmask1 & ~lmask0 &  req2  & ~req1)
+           | (~lcomreq & ~lmask1 &  lmask0 &  req2)
+           | (~lcomreq &  lmask1 & ~lmask0 & ~req3 &  req2  & ~req1 & ~req0)
+           | (~lcomreq &  lmask1 &  lmask0 &  req2 & ~req1 & ~req0)
+           | ( lcomreq &  lgnt2);
+    lgnt3 <= (~lcomreq & ~lmask1 & ~lmask0 & req3  & ~req2 & ~req1)
+           | (~lcomreq & ~lmask1 &  lmask0 & req3  & ~req2)
+           | (~lcomreq &  lmask1 & ~lmask0 & req3)
+           | (~lcomreq &  lmask1 &  lmask0 & req3  & ~req2 & ~req1 & ~req0)
+           | ( lcomreq & lgnt3);
 end 
 
 //----------------------------------------------------
@@ -76,19 +76,19 @@ end
 //----------------------------------------------------
 assign beg = (req3 | req2 | req1 | req0) & ~lcomreq;
 always @ (posedge clk)
-begin                                     
-  lasmask <= (beg & ~ledge & ~lasmask);
-  ledge   <= (beg & ~ledge &  lasmask) 
-          |  (beg &  ledge & ~lasmask);
-end 
+begin
+    lasmask <= (beg & ~ledge & ~lasmask);
+    ledge   <= (beg & ~ledge &  lasmask) 
+             | (beg &  ledge & ~lasmask);
+end
 
 //----------------------------------------------------
 // comreq logic.
 //----------------------------------------------------
 assign lcomreq = ( req3 & lgnt3 )
-                | ( req2 & lgnt2 )
-                | ( req1 & lgnt1 )
-                | ( req0 & lgnt0 );
+               | ( req2 & lgnt2 )
+               | ( req1 & lgnt1 )
+               | ( req0 & lgnt0 );
 
 //----------------------------------------------------
 // Encoder logic.
@@ -100,14 +100,14 @@ assign  lgnt =  {(lgnt3 | lgnt2),(lgnt3 | lgnt1)};
 //----------------------------------------------------
 always @ (posedge clk )
 if( rst ) begin
-  lmask1 <= 0;
-  lmask0 <= 0;
+    lmask1 <= 0;
+    lmask0 <= 0;
 end else if(lasmask) begin
-  lmask1 <= lgnt[1];
-  lmask0 <= lgnt[0];
+    lmask1 <= lgnt[1];
+    lmask0 <= lgnt[0];
 end else begin
-  lmask1 <= lmask1;
-  lmask0 <= lmask0;
+    lmask1 <= lmask1;
+    lmask0 <= lmask0;
 end 
 
 assign comreq = lcomreq;
