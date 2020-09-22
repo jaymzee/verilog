bd2fa980e9e4ffa44ccdc7bc2835a3df2da434dd
jaymzee <jaymzee@users.noreply.github.com>
2020-09-22 14:54:51 -0400

    counter - always_ff non-blocking assignments

--- counter/counter.sv
+++ counter/counter.sv
@@ -10,12 +10,12 @@ parameter WIDTH = 4;
 
 always_ff @(posedge clk, negedge rst_)
     if (!rst_)
-        count = 0;
+        count <= 0;
     else
         if (!en_)
             if (up)
-                count = count + 1;
+                count <= count + 1;
             else
-                count = count - 1;
+                count <= count - 1;
 
 endmodule
