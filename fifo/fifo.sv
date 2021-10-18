4bd2064b95a89df8ec5a754c3a105897d2885da2
jaymzee <jaymzee@users.noreply.github.com>
2021-10-18 18:28:12 -0400

    fifo: seq logic should use non-blocking assignment

--- fifo/fifo.sv
+++ fifo/fifo.sv
@@ -12,7 +12,7 @@ module fifo (
 
 parameter DWIDTH = 8;
 parameter AWIDTH = 3;
-localparam DEPTH = 2 ** AWIDTH;
+localparam DEPTH = 1 << AWIDTH;
 
 logic [AWIDTH:0] count = 0;
 logic [AWIDTH-1:0] rd_addr = 0, wr_addr = 0;
@@ -21,22 +21,22 @@ logic [DWIDTH-1:0] fifo[DEPTH-1:0];
 assign empty = (count == 0) ? 1 : 0;
 assign full = (count == DEPTH) ? 1 : 0;
 
-always @(posedge clk or posedge rst) begin
+always @(posedge clk, posedge rst) begin
     if (rst) begin
-        rd_addr = 0;
-        wr_addr = 0;
-        count = 0;
+        rd_addr <= 0;
+        wr_addr <= 0;
+        count <= 0;
     end
 
     if (en)
         if (rd && count > 0) begin
-            dataOut = fifo[rd_addr];
-            rd_addr = (rd_addr + 1) % DEPTH;
-            count = count - 1;
+            dataOut <= fifo[rd_addr];
+            rd_addr <= (rd_addr + 1) % DEPTH;
+            count <= count - 1;
         end else if (wr && count < DEPTH) begin
-            fifo[wr_addr] = dataIn;
-            wr_addr = (wr_addr + 1) % DEPTH;
-            count = count + 1;
+            fifo[wr_addr] <= dataIn;
+            wr_addr <= (wr_addr + 1) % DEPTH;
+            count <= count + 1;
         end
 end
 
