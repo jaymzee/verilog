ffd4826973683b6264501dc77fce8a615a5615a8
jaymzee <jaymzee@users.noreply.github.com>
2021-10-20 06:59:55 -0400

    risc16

---
+++ cpu/risc16/test_risc16.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+`include "params.v"
+`include "risc16.v"
+
+// fpga4student.com 
+// FPGA projects, VHDL projects, Verilog projects 
+// Verilog code for RISC Processor 
+// Verilog testbench code to test the processor
+module test_Risc_16_bit;
+
+ // Inputs
+ reg clk;
+
+ // Instantiate the Unit Under Test (UUT)
+ Risc_16_bit uut (
+  .clk(clk)
+ );
+
+ initial 
+  begin
+    $dumpfile("risc16.vcd");
+    $dumpvars(0, test_Risc_16_bit);
+   clk <=0;
+   `simulation_time;
+   $finish;
+  end
+
+ always 
+  begin
+   #5 clk = ~clk;
+  end
+
+endmodule
